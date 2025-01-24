c0565d5d6c7962b1ebae0d61f10f32ea77a2db9a
naeimsalib <naeimsalib@yahoo.com>
2025-01-24 12:31:55 -0500

    Update CSS look and styling

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,21 +4,30 @@ import './Footer.css';
 export default function Footer({ user }) {
   return (
     <footer className="Footer">
-      <nav>
-        {user ? (
-          <>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/workouts">Workouts</NavLink>
-            <NavLink to="/community">Community</NavLink>
-          </>
-        ) : (
-          <>
-            <NavLink to="/login">Log In</NavLink>
-            <NavLink to="/signup">Sign Up</NavLink>
-          </>
-        )}
-      </nav>
-      <p>&copy; {new Date().getFullYear()} GainzTracker. All Rights Reserved.</p>
+      <div className="footer-top">
+        <h2 className="footer-logo">GainzTracker</h2>
+        <div className="footer-links">
+          <h3>Links</h3>
+          <nav>
+            {user ? (
+              <>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/workouts">Workouts</NavLink>
+                <NavLink to="/community">Community</NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink to="/signup">Sign Up</NavLink>
+                <NavLink to="/login">Sign In</NavLink>
+              </>
+            )}
+          </nav>
+        </div>
+      </div>
+      <hr className="footer-line" />
+      <p className="footer-copyright">
+        &copy; {new Date().getFullYear()} GainzTracker
+      </p>
     </footer>
   );
 }
