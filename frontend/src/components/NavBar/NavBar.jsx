5e436ec6b30c1fc487ea6fe6c6ee9d9bae16542a
naeimsalib <naeimsalib@yahoo.com>
2025-01-24 12:52:19 -0500

    Update Navbar to include All required buttons, as well as update CSS to have a clean layout

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,48 @@
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { logOut } from '../../services/authService';
 import './NavBar.css';
 
 export default function NavBar({ user, setUser }) {
+  const location = useLocation();
+
   function handleLogOut() {
     logOut();
     setUser(null);
   }
 
+  // Determine the title based on the page
+  let pageTitle = "GainzTracker";
+  if (location.pathname === "/signup") pageTitle = "Sign Up";
+  if (location.pathname === "/login") pageTitle = "Log In";
+
   return (
     <nav className="NavBar">
-      <NavLink to="/">Home</NavLink>
-      {user ? (
-        <>
-          <NavLink to="/workouts">Workouts</NavLink>
-          <NavLink to="/community">Community</NavLink>
-          <Link to="" onClick={handleLogOut}> Log Out </Link>
-          <span>Welcome, {user.name}</span>
-        </>
-      ) : (
-        <>
-          <NavLink to="/login">Log In</NavLink>
-          <NavLink to="/signup">Sign Up</NavLink>
-        </>
-      )}
+      <div className="navbar-left">
+        <h2>{pageTitle}</h2>
+      </div>
+      <div className="navbar-right">
+        {user ? (
+          <>
+            <NavLink to="/" className="nav-btn">Home</NavLink>
+            <NavLink to="/workouts" className="nav-btn">Workouts</NavLink>
+            <NavLink to="/exercises" className="nav-btn">Exercises</NavLink> {/* ✅ Added Exercises */}
+            <NavLink to="/community" className="nav-btn">Community</NavLink>
+            <button onClick={handleLogOut} className="nav-btn logout">Log Out</button>
+          </>
+        ) : (
+          <>
+            {location.pathname !== "/" && (
+              <NavLink to="/" className="nav-btn">Home</NavLink>
+            )}
+            {location.pathname !== "/signup" && (
+              <NavLink to="/signup" className="nav-btn">Sign Up</NavLink>
+            )}
+            {location.pathname !== "/login" && (
+              <NavLink to="/login" className="nav-btn primary">Log In</NavLink>
+            )}
+          </>
+        )}
+      </div>
     </nav>
   );
 }
