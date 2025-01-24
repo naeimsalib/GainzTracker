23f716236bb144a4a6ae61e02b0b2200ff339ed6
naeimsalib <naeimsalib@yahoo.com>
2025-01-24 11:39:47 -0500

    Add CSS for the Navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -11,21 +11,16 @@ export default function NavBar({ user, setUser }) {
   return (
     <nav className="NavBar">
       <NavLink to="/">Home</NavLink>
-      &nbsp; | &nbsp;
       {user ? (
         <>
           <NavLink to="/workouts">Workouts</NavLink>
-          &nbsp; | &nbsp;
           <NavLink to="/community">Community</NavLink>
-          &nbsp; | &nbsp;
           <Link to="" onClick={handleLogOut}> Log Out </Link>
-          &nbsp; | &nbsp;
           <span>Welcome, {user.name}</span>
         </>
       ) : (
         <>
           <NavLink to="/login">Log In</NavLink>
-          &nbsp; | &nbsp;
           <NavLink to="/signup">Sign Up</NavLink>
         </>
       )}
