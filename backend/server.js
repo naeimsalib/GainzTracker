a94644332744b728075945e3e372679a010a36d9
naeimsalib <naeimsalib@yahoo.com>
2025-01-25 15:10:29 -0500

    Connected workout routes to server

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.use(require('./middleware/ensureLoggedIn'));
 // API Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/exercises', require('./routes/exercises'));
+app.use('/api/workouts', require('./routes/workout'));
 
 // Use a "catch-all" route to deliver the frontend's production index.html
 app.get('*', function (req, res) {
