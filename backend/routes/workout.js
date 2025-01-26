2f0fefb85a153d389b2fe9eaf1c16390b037ed23
naeimsalib <naeimsalib@yahoo.com>
2025-01-25 19:02:55 -0500

    Added routes for sharing and fetching community workouts and exercises

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -18,4 +18,10 @@ router.put('/:id', ensureLoggedIn, workoutCtrl.updateWorkout);
 // DELETE a workout
 router.delete('/:id', ensureLoggedIn, workoutCtrl.deleteWorkout);
 
+// PUT to share a workout
+router.put('/:id/share', ensureLoggedIn, workoutCtrl.shareWorkout);
+
+// GET all shared workouts
+router.get('/community', workoutCtrl.getSharedWorkouts);
+
 module.exports = router;
