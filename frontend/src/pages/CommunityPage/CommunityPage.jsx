b270c117b75c3cb79e89ee06508d09219131b3a8
naeimsalib <naeimsalib@yahoo.com>
2025-01-25 19:04:23 -0500

    Implemented Community Page UI

--- frontend/src/pages/CommunityPage/CommunityPage.jsx
+++ frontend/src/pages/CommunityPage/CommunityPage.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import { getSharedWorkouts } from "../../services/workoutService";
+import { getSharedExercises } from "../../services/exerciseService";
+import "./CommunityPage.css";
+
 export default function CommunityPage() {
-    return (
-      <div>
-        <h1>Community Page</h1>
-      </div>
-    );
-  }
-  
+  const [workouts, setWorkouts] = useState([]);
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    async function fetchCommunityData() {
+      setWorkouts(await getSharedWorkouts());
+      setExercises(await getSharedExercises());
+    }
+    fetchCommunityData();
+  }, []);
+
+  return (
+    <div className="CommunityPage">
+      <h1>Community Workouts & Exercises</h1>
+      <h2>Shared Workouts</h2>
+      {workouts.map((workout) => (
+        <div key={workout._id}>{workout.title} by {workout.user.name}</div>
+      ))}
+
+      <h2>Shared Exercises</h2>
+      {exercises.map((exercise) => (
+        <div key={exercise._id}>{exercise.name} by {exercise.user.name}</div>
+      ))}
+    </div>
+  );
+}
