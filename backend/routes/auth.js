3e590e8cd4d482ec48ae357c42a007693d197129
naeimsalib <naeimsalib@yahoo.com>
2025-01-31 11:23:52 -0500

    Fix Signup Page issues with auth issues

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,6 @@ const authCtrl = require('../controllers/auth');
 // POST /api/auth/signup
 router.post('/signup', authCtrl.signUp);
 // POST /api/auth/login
-router.post('/login', authCtrl.login);
+router.post('/login', authCtrl.logIn);
 
 module.exports = router;
